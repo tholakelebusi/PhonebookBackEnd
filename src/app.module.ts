0d87346c096a541f6efbe4f28c4d828e0b657a12
Cossa Tholakele <cossa@thedigitalacademy.co.za>
2021-09-15 11:39:08 +0200

    replasing connection string

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { PhonebookModule } from './phonebook/phonebook.module';
 
 @Module({
 	imports: [
-		MongooseModule.forRoot("mongodb://localhost:27017/PHONEBOOK_DB", { autoCreate: true }),
+		MongooseModule.forRoot("mongodb+srv://tholakelebusi:123456@cluster0.blwxz.mongodb.net/Phonebook", { autoCreate: true }),
 		PhonebookModule,
 	],
 	controllers: [],
